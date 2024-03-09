33d6eef4d3bef0a259e9e666fef2f0e1e1e79a83
Daeun Kim <jkde7721@naver.com>
2024-03-09 17:06:03 +0900

    Init Project

---
+++ src/main/java/com/project/foradhd/ForadhdApplication.java
@@ -0,0 +1,13 @@
+package com.project.foradhd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForadhdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForadhdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/foradhd/ForadhdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.foradhd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForadhdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
