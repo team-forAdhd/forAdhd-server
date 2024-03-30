17e01a36350219e737252115dcc499daed4f8ffa
Daeun Kim <jkde7721@naver.com>
2024-03-30 23:33:51 +0900

    add: 유저 권한 "ROLE_" prefix 추가
    #9

--- src/main/java/com/project/foradhd/domain/auth/business/userdetails/impl/UserDetailsImpl.java
+++ src/main/java/com/project/foradhd/domain/auth/business/userdetails/impl/UserDetailsImpl.java
@@ -1,9 +1,9 @@
 package com.project.foradhd.domain.auth.business.userdetails.impl;
 
+import static org.springframework.security.core.authority.AuthorityUtils.createAuthorityList;
+
 import com.project.foradhd.domain.user.persistence.entity.User;
-import java.util.List;
 import lombok.Getter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 @Getter
 public class UserDetailsImpl extends org.springframework.security.core.userdetails.User {
@@ -11,7 +11,7 @@ public class UserDetailsImpl extends org.springframework.security.core.userdetai
     private final String userId;
 
     public UserDetailsImpl(User user) {
-        super(user.getEmail(), user.getPassword(), List.of(new SimpleGrantedAuthority("USER")));
+        super(user.getEmail(), user.getPassword(), createAuthorityList("ROLE_USER"));
         this.userId = user.getId();
     }
 }
