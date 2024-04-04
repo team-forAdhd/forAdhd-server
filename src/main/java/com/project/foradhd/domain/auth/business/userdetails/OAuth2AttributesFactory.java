ba54d8546c848112230b9f82bc57de9e16d239b1
Daeun Kim <jkde7721@naver.com>
2024-04-05 06:47:38 +0900

    feat: sns별 response 파싱하는 Factory 구현
    - Provider 기준 switch하여 구현
    
    #12

---
+++ src/main/java/com/project/foradhd/domain/auth/business/userdetails/OAuth2AttributesFactory.java
@@ -0,0 +1,31 @@
+package com.project.foradhd.domain.auth.business.userdetails;
+
+import com.project.foradhd.domain.auth.business.userdetails.impl.AppleOAuth2Attributes;
+import com.project.foradhd.domain.auth.business.userdetails.impl.FacebookOAuth2Attributes;
+import com.project.foradhd.domain.auth.business.userdetails.impl.KakaoOAuth2Attributes;
+import com.project.foradhd.domain.auth.business.userdetails.impl.NaverOAuth2Attributes;
+import com.project.foradhd.domain.user.persistence.enums.Provider;
+import java.util.Map;
+
+public abstract class OAuth2AttributesFactory {
+
+    public static OAuth2Attributes of(String registrationId, String nameAttributesKey,
+        Map<String, Object> attributes) {
+        Provider provider = Provider.from(registrationId);
+        switch (provider) {
+            case NAVER -> {
+                return NaverOAuth2Attributes.of(nameAttributesKey, attributes);
+            }
+            case KAKAO -> {
+                return KakaoOAuth2Attributes.of(nameAttributesKey, attributes);
+            }
+            case FACEBOOK -> {
+                return FacebookOAuth2Attributes.of(nameAttributesKey, attributes);
+            }
+            case APPLE -> {
+                return AppleOAuth2Attributes.of(nameAttributesKey, attributes);
+            }
+            default -> throw new RuntimeException("유효하지 않은 SNS 입니다.");
+        }
+    }
+}
--- src/main/java/com/project/foradhd/domain/user/persistence/enums/Provider.java
+++ src/main/java/com/project/foradhd/domain/user/persistence/enums/Provider.java
@@ -2,5 +2,9 @@ package com.project.foradhd.domain.user.persistence.enums;
 
 public enum Provider {
 
-    FOR_A, NAVER, KAKAO, FACEBOOK, APPLE
+    FOR_A, NAVER, KAKAO, FACEBOOK, APPLE;
+
+    public static Provider from(String value) {
+        return Provider.valueOf(value.toUpperCase());
+    }
 }
