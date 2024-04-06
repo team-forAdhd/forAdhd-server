26fe15951c59ea0791cbd2d8dc603634c2c9c402
Daeun Kim <jkde7721@naver.com>
2024-04-06 19:56:26 +0900

    add: 소셜 로그인 시 추가 정보 입력하여 회원가입 완료한 유저인지 나타내는 응답값 추가
    #12

--- src/main/java/com/project/foradhd/domain/auth/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/project/foradhd/domain/auth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -4,13 +4,16 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import com.project.foradhd.domain.auth.business.service.JwtService;
 import com.project.foradhd.domain.auth.business.userdetails.impl.OAuth2UserImpl;
-import com.project.foradhd.domain.auth.web.dto.response.LoginResponse;
+import com.project.foradhd.domain.auth.web.dto.response.OAuth2LoginResponse;
+import com.project.foradhd.domain.user.persistence.enums.Role;
 import com.project.foradhd.global.util.JsonUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +21,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+    private static final GrantedAuthority SIGNED_UP_USER_AUTHORITY = new SimpleGrantedAuthority(Role.USER.getName());
     private final JwtService jwtService;
 
     @Override
@@ -27,10 +31,11 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
         String accessToken = jwtService.generateAccessToken(oAuth2User.getUserId(), oAuth2User.getEmail(),
             oAuth2User.getAuthorities());
         String refreshToken = jwtService.generateRefreshToken(oAuth2User.getUserId());
+        boolean isSignedUpUser = oAuth2User.getAuthorities().contains(SIGNED_UP_USER_AUTHORITY);
 
         //TODO: RT 저장소에 저장
-        LoginResponse loginResponse = new LoginResponse(accessToken, refreshToken);
+        OAuth2LoginResponse oAuth2LoginResponse = new OAuth2LoginResponse(accessToken, refreshToken, isSignedUpUser);
         response.setContentType(APPLICATION_JSON_VALUE);
-        response.getWriter().write(JsonUtil.writeValueAsString(loginResponse));
+        response.getWriter().write(JsonUtil.writeValueAsString(oAuth2LoginResponse));
     }
 }
