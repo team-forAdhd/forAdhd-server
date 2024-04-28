bd0024fbe56f94b71cee8756c40ff5a09e9eed6d
Daeun Kim <jkde7721@naver.com>
2024-04-28 17:31:23 +0900

    refactor: ErrorResponse에서 직접 ResponseEntity 객체 생성하는 static 메소드 정의
    - ErrorCode로부터 ResponseEntity<ErrorResponse> 객체 생성하는 boiler plate 코드 제거 가능
    
    #21

--- src/main/java/com/project/foradhd/global/exception/dto/response/ErrorResponse.java
+++ src/main/java/com/project/foradhd/global/exception/dto/response/ErrorResponse.java
@@ -1,9 +1,11 @@
 package com.project.foradhd.global.exception.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.project.foradhd.global.exception.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -26,5 +28,25 @@ public class ErrorResponse {
 
         private String fieldName;
         private String message;
+    }
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(errorCode.getStatus().value())
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .build();
+        return new ResponseEntity<>(errorResponse, errorCode.getStatus());
+    }
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode,
+                                                    List<ValidationErrorResponse> validationErrors) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(errorCode.getStatus().value())
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .validationErrors(validationErrors)
+                .build();
+        return new ResponseEntity<>(errorResponse, errorCode.getStatus());
     }
 }
--- src/main/java/com/project/foradhd/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/project/foradhd/global/exception/handler/GlobalExceptionHandler.java
@@ -8,16 +8,12 @@ import com.project.foradhd.global.exception.InternalSystemException;
 import com.project.foradhd.global.exception.dto.response.ErrorResponse;
 import com.project.foradhd.global.exception.dto.response.ErrorResponse.ValidationErrorResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.format.DateTimeParseException;
 import java.util.Arrays;
@@ -26,7 +22,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     private static final String DEFAULT_INVALID_FORMAT_ERROR_MESSAGE_TEMPLATE = "Cannot deserialize value of type `%s` from String \"%s\"";
     private static final String ENUM_INVALID_FORMAT_ERROR_MASSAGE_TEMPLATE = ": not one of the values accepted for Enum class: %s";
@@ -35,33 +31,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
         log.error("Exception occurred while processing request", e);
-
-        ErrorCode errorCode = e.getErrorCode();
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(errorCode.getStatus().value())
-                .code(errorCode.name())
-                .message(errorCode.getMessage())
-                .build();
-        return new ResponseEntity<>(errorResponse, errorCode.getStatus());
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
 
     @ExceptionHandler(InternalSystemException.class)
-    public ResponseEntity<ErrorResponse> handleSystemException(InternalSystemException e) {
+    public ResponseEntity<ErrorResponse> handleInternalSystemException(InternalSystemException e) {
         Throwable cause = e.getCause();
         log.error("Exception occurred while processing request", cause);
-
-        ErrorCode errorCode = ErrorCode.SYSTEM_ERROR;
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(errorCode.getStatus().value())
-                .code(errorCode.name())
-                .message(errorCode.getMessage())
-                .build();
-        return new ResponseEntity<>(errorResponse, errorCode.getStatus());
+        return ErrorResponse.toResponseEntity(ErrorCode.SYSTEM_ERROR);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers,
-                                                                HttpStatusCode status, WebRequest request) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.error("Invalid request content", e);
         List<ValidationErrorResponse> validationErrors = e.getAllErrors().stream()
                 .map(objectError -> {
@@ -71,22 +52,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                     return new ValidationErrorResponse(objectError.getObjectName(), objectError.getDefaultMessage());
                 })
                 .toList();
-
-        ErrorCode errorCode = ErrorCode.INVALID_REQUEST;
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(errorCode.getStatus().value())
-                .code(errorCode.name())
-                .message(errorCode.getMessage())
-                .validationErrors(validationErrors)
-                .build();
-        return new ResponseEntity<>(errorResponse, errorCode.getStatus());
+        return ErrorResponse.toResponseEntity(ErrorCode.INVALID_REQUEST, validationErrors);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException e, HttpHeaders headers, 
-                                                                HttpStatusCode status, WebRequest request) {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
         log.error("Invalid request content", e);
-        ErrorCode errorCode = ErrorCode.INVALID_REQUEST;
 
         //Check if the exception is due to JSON parsing error
         if (e.getCause() instanceof InvalidFormatException cause) {
@@ -96,23 +67,10 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                     .map(JsonMappingException.Reference::getFieldName)
                     .collect(Collectors.joining(", "));
             String message = generateValidationMessage(value, targetType, cause);
-
-            ErrorResponse errorResponse = ErrorResponse.builder()
-                    .status(errorCode.getStatus().value())
-                    .code(errorCode.name())
-                    .message(errorCode.getMessage())
-                    .validationErrors(List.of(
-                            new ValidationErrorResponse(fieldName, message)))
-                    .build();
-            return new ResponseEntity<>(errorResponse, errorCode.getStatus());
+            List<ValidationErrorResponse> validationErrors = List.of(new ValidationErrorResponse(fieldName, message));
+            return ErrorResponse.toResponseEntity(ErrorCode.INVALID_REQUEST, validationErrors);
         }
-
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(errorCode.getStatus().value())
-                .code(errorCode.name())
-                .message(errorCode.getMessage())
-                .build();
-        return new ResponseEntity<>(errorResponse, errorCode.getStatus());
+        return ErrorResponse.toResponseEntity(ErrorCode.INVALID_REQUEST);
     }
 
     private String generateValidationMessage(Object value, Class<?> targetType, Throwable throwable) {
