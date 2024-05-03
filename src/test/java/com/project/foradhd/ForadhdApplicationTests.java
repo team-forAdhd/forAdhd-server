ecadc06fd2dff14fabdc56ecbafd7978ea943baf
Daeun Kim <jkde7721@naver.com>
2024-05-03 19:46:19 +0900

    test: @SpringBootTest 어노테이션 선언된 테스트에 @IntegrationTag 추가
    - @SpringBootTest는 스프링 관련 클래스를 모두 빈으로 등록하는 무거운 테스트이므로 @IntegrationTag를 선언하여 통합 테스트 시에만 실행되도록 함
    
    #24

--- src/test/java/com/project/foradhd/ForadhdApplicationTests.java
+++ src/test/java/com/project/foradhd/ForadhdApplicationTests.java
@@ -1,8 +1,10 @@
 package com.project.foradhd;
 
+import com.project.foradhd.tags.IntegrationTag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@IntegrationTag
 @SpringBootTest
 class ForadhdApplicationTests {
 
