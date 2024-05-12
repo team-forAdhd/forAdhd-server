79a0e7c84bb03832489a34b6539f25c7c96f12e2
Daeun Kim <jkde7721@naver.com>
2024-05-12 13:53:13 +0900

    fix: BusinessException 에러 메시지 설정하도록 수정
    - 설정 안하면 서버 로그에 안남음
    
    #28

--- src/main/java/com/project/foradhd/global/exception/BusinessException.java
+++ src/main/java/com/project/foradhd/global/exception/BusinessException.java
@@ -1,11 +1,14 @@
 package com.project.foradhd.global.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class BusinessException extends RuntimeException {
 
     private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
 }
