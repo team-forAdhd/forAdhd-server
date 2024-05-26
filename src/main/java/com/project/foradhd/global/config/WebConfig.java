356e65b33c5eba9d6872140c20216c5c27c8170d
Daeun Kim <jkde7721@naver.com>
2024-05-27 07:28:50 +0900

    del: 기존 custom paging 관련 구현 제거
    - 그냥 스프링이 제공해주는 것 사용하자
    - Spring Data JPA에 대한 의존성 변경 없음
    
    #28

--- src/main/java/com/project/foradhd/global/config/WebConfig.java
+++ src/main/java/com/project/foradhd/global/config/WebConfig.java
@@ -1,16 +1,12 @@
 package com.project.foradhd.global.config;
 
-import com.project.foradhd.global.paging.argumentresolver.PagingRequestArgumentResolver;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -36,10 +32,5 @@ public class WebConfig implements WebMvcConfigurer {
         LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
         localValidatorFactoryBean.setValidationMessageSource(validationMessageSource());
         return localValidatorFactoryBean;
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new PagingRequestArgumentResolver());
     }
 }
