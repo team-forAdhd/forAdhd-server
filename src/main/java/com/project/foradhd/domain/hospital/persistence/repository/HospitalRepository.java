446785ea557ebc65e4e7ab99915752414d7c8888
Daeun Kim <jkde7721@naver.com>
2024-05-27 07:32:31 +0900

    feat: 내 주변 병원 목록 조회 repository 구현
    #28

--- src/main/java/com/project/foradhd/domain/hospital/persistence/repository/HospitalRepository.java
+++ src/main/java/com/project/foradhd/domain/hospital/persistence/repository/HospitalRepository.java
@@ -1,13 +1,14 @@
 package com.project.foradhd.domain.hospital.persistence.repository;
 
 import com.project.foradhd.domain.hospital.persistence.entity.Hospital;
+import com.project.foradhd.domain.hospital.persistence.repository.custom.HospitalRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface HospitalRepository extends JpaRepository<Hospital, String> {
+public interface HospitalRepository extends JpaRepository<Hospital, String>, HospitalRepositoryCustom {
 
     @Query("""
         select h
