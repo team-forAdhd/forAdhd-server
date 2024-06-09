e1a73755215386fce142e3ad4314c7c240043a14
Daeun Kim <jkde7721@naver.com>
2024-06-09 15:15:58 +0900

    refactor: 소셜 로그인 타입에 따라 유저 정보 파싱하는 OAuth2AttributesFactory 클래스의 valueOf 메소드로 OAuth2UserRequest 객체 넘기도록 리팩토링
    - 구글 로그인 시 유저 추가정보(생년월일, 성별 등)를 얻기 위해 추가적인 people api 호출이 필요하기 때문(OAuth2UserRequest 객체로부터 엑세스 토큰 가져와야 함)
    
    #33

--- src/main/java/com/project/foradhd/domain/auth/business/service/impl/OAuth2UserServiceImpl.java
+++ src/main/java/com/project/foradhd/domain/auth/business/service/impl/OAuth2UserServiceImpl.java
@@ -29,14 +29,13 @@ public class OAuth2UserServiceImpl extends DefaultOAuth2UserService {
 
     @Transactional
     @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        String nameAttributeKey = userRequest.getClientRegistration().getProviderDetails()
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
+        String nameAttributeKey = oAuth2UserRequest.getClientRegistration().getProviderDetails()
             .getUserInfoEndpoint().getUserNameAttributeName();
         Map<String, Object> attributes = oAuth2User.getAttributes();
 
-        OAuth2Attributes oAuth2Attributes = OAuth2AttributesFactory.valueOf(registrationId, nameAttributeKey, attributes);
+        OAuth2Attributes oAuth2Attributes = OAuth2AttributesFactory.valueOf(oAuth2UserRequest, nameAttributeKey, attributes);
         User user = getSocialLoginedUser(oAuth2Attributes);
         return new OAuth2UserImpl(attributes, nameAttributeKey, user);
     }
--- src/main/java/com/project/foradhd/domain/auth/business/userdetails/OAuth2AttributesFactory.java
+++ src/main/java/com/project/foradhd/domain/auth/business/userdetails/OAuth2AttributesFactory.java
@@ -3,6 +3,7 @@ package com.project.foradhd.domain.auth.business.userdetails;
 import com.project.foradhd.domain.auth.business.userdetails.impl.*;
 import com.project.foradhd.domain.user.persistence.enums.Provider;
 import com.project.foradhd.global.exception.BusinessException;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 
 import java.util.Map;
 
@@ -10,8 +11,9 @@ import static com.project.foradhd.global.exception.ErrorCode.NOT_SUPPORTED_SNS_T
 
 public abstract class OAuth2AttributesFactory {
 
-    public static OAuth2Attributes valueOf(String registrationId, String nameAttributesKey,
-        Map<String, Object> attributes) {
+    public static OAuth2Attributes valueOf(OAuth2UserRequest oAuth2UserRequest, String nameAttributesKey,
+                                        Map<String, Object> attributes) {
+        String registrationId = oAuth2UserRequest.getClientRegistration().getRegistrationId();
         Provider provider = Provider.from(registrationId)
             .orElseThrow(() -> new BusinessException(NOT_SUPPORTED_SNS_TYPE));
         switch (provider) {
@@ -22,7 +24,8 @@ public abstract class OAuth2AttributesFactory {
                 return KakaoOAuth2Attributes.of(nameAttributesKey, attributes);
             }
             case GOOGLE -> {
-                return GoogleOAuth2Attributes.of(nameAttributesKey, attributes);
+                String accessToken = oAuth2UserRequest.getAccessToken().getTokenValue();
+                return GoogleOAuth2Attributes.of(nameAttributesKey, attributes, accessToken);
             }
             case APPLE -> {
                 return AppleOAuth2Attributes.of(nameAttributesKey, attributes);
