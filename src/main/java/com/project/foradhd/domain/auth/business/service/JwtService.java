93d3de0537d560e263dfc4398bf640f91346b1b4
Daeun Kim <jkde7721@naver.com>
2024-06-23 16:22:58 +0900

    feat: 애플 id 토큰 검증 후 디코딩하여 필요한 정보 조회해오는 로직 구현
    #35

--- src/main/java/com/project/foradhd/domain/auth/business/service/JwtService.java
+++ src/main/java/com/project/foradhd/domain/auth/business/service/JwtService.java
@@ -1,6 +1,10 @@
 package com.project.foradhd.domain.auth.business.service;
 
+import java.security.Key;
 import java.util.Collection;
+import java.util.Map;
+
+import io.jsonwebtoken.Claims;
 import org.springframework.security.core.GrantedAuthority;
 
 public interface JwtService {
@@ -8,11 +12,16 @@ public interface JwtService {
     String generateAccessToken(String userId, String email, Collection<GrantedAuthority> authorities);
     String generateRefreshToken(String userId);
     void validateTokenExpiry(String token);
+    void validateTokenExpiry(String token, Key key);
     void validateTokenForm(String token);
     boolean isValidTokenExpiry(String token);
     boolean isValidTokenForm(String token);
     String getSubject(String token);
     Collection<GrantedAuthority> getAuthorities(String token);
+    Map<String, Object> decodeHeader(String token);
+    Map<String, Object> decodePayload(String token);
+    Claims parseToken(String token, Key key);
+    Claims parseExpiredToken(String token, Key key);
     void saveRefreshToken(String userId, String refreshToken);
     void deleteRefreshToken(String userId);
     boolean existsSavedRefreshToken(String userId, String refreshToken);
--- src/main/java/com/project/foradhd/domain/auth/business/service/impl/AppleOAuth2UserAttributesServiceImpl.java
+++ src/main/java/com/project/foradhd/domain/auth/business/service/impl/AppleOAuth2UserAttributesServiceImpl.java
@@ -1,34 +1,32 @@
 package com.project.foradhd.domain.auth.business.service.impl;
 
+import com.project.foradhd.domain.auth.business.service.AppleIdTokenValidator;
+import com.project.foradhd.domain.auth.business.service.JwtService;
 import com.project.foradhd.domain.auth.business.service.OAuth2UserAttributesService;
-import com.project.foradhd.global.util.JsonUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.Map;
 
 import static org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames.ID_TOKEN;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class AppleOAuth2UserAttributesServiceImpl implements OAuth2UserAttributesService {
+
+    private final AppleIdTokenValidator appleIdTokenValidator;
+    private final JwtService jwtService;
 
     @Override
     public Map<String, Object> getAttributes(OAuth2UserRequest oAuth2UserRequest, Map<String, Object> attributes) {
         String idToken = oAuth2UserRequest.getAdditionalParameters().get(ID_TOKEN).toString();
-        attributes.putAll(decodeIdToken(idToken));
+        appleIdTokenValidator.validate(idToken);
+
+        Map<String, Object> decodedPayload = jwtService.decodePayload(idToken);
+        attributes.putAll(decodedPayload);
         return attributes;
-    }
-
-    public Map<String, Object> decodeIdToken(String idToken) {
-        String[] encodedPayload = idToken.split("\\.");
-        Base64.Decoder decoder = Base64.getUrlDecoder();
-
-        byte[] decodedBytes = decoder.decode(encodedPayload[1].getBytes(StandardCharsets.UTF_8));
-        String decodedPayload = new String(decodedBytes, StandardCharsets.UTF_8);
-        return JsonUtil.readValue(decodedPayload, Map.class);
     }
 }
