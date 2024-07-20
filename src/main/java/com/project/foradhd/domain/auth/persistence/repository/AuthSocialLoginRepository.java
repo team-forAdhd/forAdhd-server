5c6f56c6cbbacf09723de0c552cf209256693dfd
Daeun Kim <jkde7721@naver.com>
2024-07-20 18:01:31 +0900

    feat: 유저 탈퇴 로직 구현
    #41

--- src/main/java/com/project/foradhd/domain/auth/persistence/repository/AuthSocialLoginRepository.java
+++ src/main/java/com/project/foradhd/domain/auth/persistence/repository/AuthSocialLoginRepository.java
@@ -4,8 +4,15 @@ import com.project.foradhd.domain.auth.persistence.entity.AuthSocialLogin;
 import com.project.foradhd.domain.user.persistence.enums.Provider;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AuthSocialLoginRepository extends JpaRepository<AuthSocialLogin, String> {
 
     Optional<AuthSocialLogin> findByProviderAndExternalUserId(Provider provider, String externalUserId);
+
+    @Modifying
+    @Query("delete from AuthSocialLogin asl where asl.user.id = :userId")
+    void deleteByUserId(@Param("userId") String userId);
 }
