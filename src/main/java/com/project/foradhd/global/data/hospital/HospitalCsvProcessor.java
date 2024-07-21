473ddc28ecf34326959d398383c31bd106b57019
Daeun Kim <jkde7721@naver.com>
2024-07-22 04:01:58 +0900

    feat: 병원, 의사 CSV → DB 저장 로직 마무리
    - 생성, 수정, 삭제 로직 구현
    - 생성 시 auto generated된 UUID를 결과 CSV 파일에 write
    
    #43

--- src/main/java/com/project/foradhd/global/data/hospital/HospitalCsvProcessor.java
+++ src/main/java/com/project/foradhd/global/data/hospital/HospitalCsvProcessor.java
@@ -12,19 +12,37 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static com.project.foradhd.global.util.StringUtil.addPrefix;
 import static com.project.foradhd.global.util.StringUtil.strip;
 
 @RequiredArgsConstructor
 @Component
 public class HospitalCsvProcessor implements CommandLineRunner {
 
-    private static final String HOSPITAL_CSV_PATH = "/static/csv/hospital.csv";
-    private static final String DOCTOR_CSV_PATH = "/static/csv/doctor.csv";
+    private static final String HOSPITAL_CSV_INPUT_PATH = "/static/csv/hospital.csv";
+    private static final String DOCTOR_CSV_INPUT_PATH = "/static/csv/doctor.csv";
+    private static final String HOSPITAL_CSV_OUTPUT_PATH = "src/main/resources/static/csv/hospital_result.csv";
+    private static final String DOCTOR_CSV_OUTPUT_PATH = "src/main/resources/static/csv/doctor_result.csv";
+
+    private static final int HOSPITAL_ID_IDX = 0;
+    private static final int HOSPITAL_NAME_IDX = 1;
+    private static final int HOSPITAL_PHONE_IDX = 2;
+    private static final int HOSPITAL_ADDRESS_IDX = 3;
+    private static final int HOSPITAL_LATITUDE_IDX = 4;
+    private static final int HOSPITAL_LONGITUDE_IDX = 5;
+    private static final int HOSPITAL_DELETED_IDX = 6;
+
+    private static final int DOCTOR_ID_IDX = 1;
+    private static final int DOCTOR_NAME_IDX = 2;
+    private static final int DOCTOR_PROFILE_IDX = 3;
+    private static final int DOCTOR_IMAGE_IDX = 4;
+    private static final int DOCTOR_DELETED_IDX = 5;
+
+    private static final String UUID_REGEX = "^[0-9a-fA-F]{32}$";
 
     private final HospitalRepository hospitalRepository;
     private final DoctorRepository doctorRepository;
@@ -32,46 +50,70 @@ public class HospitalCsvProcessor implements CommandLineRunner {
     @Transactional
     @Override
     public void run(String... args) throws Exception {
-        List<String[]> hospitalCsvList = CSVUtil.readAll(HOSPITAL_CSV_PATH);
-        List<String[]> doctorCsvList = CSVUtil.readAll(DOCTOR_CSV_PATH);
+        List<String[]> hospitalCsvInputList = CSVUtil.readAll(HOSPITAL_CSV_INPUT_PATH);
+        List<String[]> doctorCsvInputList = CSVUtil.readAll(DOCTOR_CSV_INPUT_PATH);
+        String[] hospitalCsvHeader = CSVUtil.readHeader(HOSPITAL_CSV_INPUT_PATH);
+        String[] doctorCsvHeader = CSVUtil.readHeader(DOCTOR_CSV_INPUT_PATH);
 
-        Map<Integer, Hospital> hospitalByCsvId = hospitalCsvList.stream()
-                .collect(Collectors.toMap(hospital -> Integer.valueOf(hospital[0]), hospital -> {
+        Map<String, Hospital> hospitalMapByCsvId = hospitalCsvInputList.stream()
+                .collect(Collectors.toMap(hospital -> strip(hospital[HOSPITAL_ID_IDX]), hospital -> {
                     GeometryFactory geometryFactory = new GeometryFactory();
-                    double latitude = Double.parseDouble(hospital[2]);
-                    double longitude = Double.parseDouble(hospital[3]);
+                    double latitude = Double.parseDouble(hospital[HOSPITAL_LATITUDE_IDX]);
+                    double longitude = Double.parseDouble(hospital[HOSPITAL_LONGITUDE_IDX]);
                     Coordinate coordinate = new Coordinate(longitude, latitude);
-                    String name = strip(hospital[1]);
-                    String address = strip(hospital[4]);
-                    String phone = addPrefix(hospital[5], "0");
+
+                    String hospitalId = strip(hospital[HOSPITAL_ID_IDX]);
+                    String name = strip(hospital[HOSPITAL_NAME_IDX]);
+                    String address = strip(hospital[HOSPITAL_ADDRESS_IDX]);
+                    String phone = strip(hospital[HOSPITAL_PHONE_IDX]).replace("-", "");
+                    boolean deleted = Boolean.parseBoolean(strip(hospital[HOSPITAL_DELETED_IDX]));
 
                     return Hospital.builder()
+                            .id(hospitalId.matches(UUID_REGEX) ? hospitalId : null) //UUID 포맷인지 확인(DB에 저장된 경우인지)
                             .name(name)
                             .location(geometryFactory.createPoint(coordinate))
                             .address(address)
                             .phone(phone)
+                            .deleted(deleted)
                             .build();
-                }));
+                    }, (e1, e2) -> e1, LinkedHashMap::new));
 
-        List<Hospital> hospitalList = hospitalByCsvId.values().stream().toList();
-        List<Doctor> doctorList = doctorCsvList.stream()
+        List<Hospital> hospitalList = hospitalMapByCsvId.values().stream().toList();
+        List<Doctor> doctorList = doctorCsvInputList.stream()
                 .map(doctor -> {
-                    Integer hospitalCsvId = Integer.valueOf(strip(doctor[0].split("\\.")[0]));
-                    Hospital hospital = hospitalByCsvId.get(hospitalCsvId);
-                    String name = strip(doctor[2]);
-                    String profile = strip(doctor[3]);
-                    String image = strip(doctor[4]);
+                    String hospitalId = strip(doctor[HOSPITAL_ID_IDX]);
+                    Hospital hospital = hospitalMapByCsvId.get(hospitalId);
+                    String doctorId = strip(doctor[DOCTOR_ID_IDX]);
+                    String name = strip(doctor[DOCTOR_NAME_IDX]);
+                    String profile = strip(doctor[DOCTOR_PROFILE_IDX]);
+                    String image = strip(doctor[DOCTOR_IMAGE_IDX]);
+                    boolean deleted = Boolean.parseBoolean(strip(doctor[DOCTOR_DELETED_IDX]));
 
                     return Doctor.builder()
                             .hospital(hospital)
+                            .id(doctorId.matches(UUID_REGEX) ? doctorId : null) //UUID 포맷인지 확인(DB에 저장된 경우인지)
                             .name(name)
                             .image(image)
                             .profile(profile)
+                            .deleted(deleted)
                             .build();
                 })
                 .toList();
 
         hospitalRepository.saveAll(hospitalList);
         doctorRepository.saveAll(doctorList);
+
+        List<String[]> hospitalCsvOutputList = hospitalList.stream()
+                .map(hospital -> new String[]{hospital.getId(), hospital.getName(), hospital.getPhone(), hospital.getAddress(),
+                        String.valueOf(hospital.getLocation().getY()), String.valueOf(hospital.getLocation().getX()),
+                        String.valueOf(hospital.getDeleted() ? 1 : 0)})
+                .toList();
+        List<String[]> doctorCsvOutputList = doctorList.stream()
+                .map(doctor -> new String[]{doctor.getHospital().getId(), doctor.getId(), doctor.getName(),
+                        doctor.getProfile(), doctor.getImage(), String.valueOf(doctor.getDeleted() ? 1 : 0)})
+                .toList();
+
+        CSVUtil.writeAll(HOSPITAL_CSV_OUTPUT_PATH, hospitalCsvHeader, hospitalCsvOutputList);
+        CSVUtil.writeAll(DOCTOR_CSV_OUTPUT_PATH, doctorCsvHeader, doctorCsvOutputList);
     }
 }
