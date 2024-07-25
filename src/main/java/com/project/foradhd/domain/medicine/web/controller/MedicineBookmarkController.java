8fa52fad856b2521b64648bd079c1fb75b838800
newoceanwave <leeseohyun02@naver.com>
2024-07-25 19:35:03 +0900

    mod: 약 북마크 컨트롤러 @AuthUserId 수정

--- src/main/java/com/project/foradhd/domain/medicine/web/controller/MedicineBookmarkController.java
+++ src/main/java/com/project/foradhd/domain/medicine/web/controller/MedicineBookmarkController.java
@@ -4,6 +4,7 @@ import com.project.foradhd.domain.medicine.business.service.MedicineBookmarkServ
 import com.project.foradhd.domain.medicine.persistence.entity.MedicineBookmark;
 import com.project.foradhd.domain.medicine.web.dto.response.MedicineBookmarkResponse;
 import com.project.foradhd.domain.medicine.web.mapper.MedicineMapper;
+import com.project.foradhd.global.AuthUserId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -26,7 +27,7 @@ public class MedicineBookmarkController {
     // 약품 북마크 토글 (추가/제거)
     @PostMapping("/toggle")
     public ResponseEntity<?> toggleBookmark(
-            @RequestParam("userId") String userId,
+            @AuthUserId String userId,
             @RequestParam("medicineId") Long medicineId) {
         try {
             medicineBookmarkService.toggleBookmark(userId, medicineId);
@@ -39,7 +40,7 @@ public class MedicineBookmarkController {
     // 내가 북마크한 약 목록 조회
     @GetMapping("/my")
     public ResponseEntity<Page<MedicineBookmarkResponse>> getMyBookmarks(
-            @RequestParam("userId") String userId,
+            @AuthUserId String userId,
             @RequestParam(defaultValue = "newest") String sort,
             @PageableDefault(size = 10) Pageable pageable
     ) {
