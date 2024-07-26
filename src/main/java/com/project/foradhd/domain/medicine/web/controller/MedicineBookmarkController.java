ead5bdebf5cf2ff384369e6ddf1f5d58f3ea6a04
newoceanwave <leeseohyun02@naver.com>
2024-07-26 10:34:45 +0900

    refractor: bookmarkresponse custom paging 처리

--- src/main/java/com/project/foradhd/domain/medicine/web/controller/MedicineBookmarkController.java
+++ src/main/java/com/project/foradhd/domain/medicine/web/controller/MedicineBookmarkController.java
@@ -5,6 +5,7 @@ import com.project.foradhd.domain.medicine.persistence.entity.MedicineBookmark;
 import com.project.foradhd.domain.medicine.web.dto.response.MedicineBookmarkResponse;
 import com.project.foradhd.domain.medicine.web.mapper.MedicineMapper;
 import com.project.foradhd.global.AuthUserId;
+import com.project.foradhd.global.paging.web.dto.response.PagingResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,6 +15,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/medicine/bookmarks")
@@ -37,19 +41,24 @@ public class MedicineBookmarkController {
         }
     }
 
-    // 내가 북마크한 약 목록 조회
     @GetMapping("/my")
-    public ResponseEntity<Page<MedicineBookmarkResponse>> getMyBookmarks(
+    public ResponseEntity<MedicineBookmarkResponse.PagedMedicineBookmarkResponse> getMyBookmarks(
             @AuthUserId String userId,
             @RequestParam(defaultValue = "newest") String sort,
             @PageableDefault(size = 10) Pageable pageable
     ) {
-        Sort.Direction direction = sort.equals("oldest") ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort.Direction direction = sort.equalsIgnoreCase("oldest") ? Sort.Direction.ASC : Sort.Direction.DESC;
         Pageable sortedPageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(direction, "createdAt"));
 
         Page<MedicineBookmark> bookmarks = medicineBookmarkService.getBookmarksByUser(userId, sortedPageable);
-        Page<MedicineBookmarkResponse> bookmarkResponses = bookmarks.map(medicineMapper::toResponseDto);
+        List<MedicineBookmarkResponse> bookmarkResponses = bookmarks.map(medicineMapper::toResponseDto).getContent();
 
-        return ResponseEntity.ok(bookmarkResponses);
+        PagingResponse pagingResponse = PagingResponse.from(bookmarks);
+        MedicineBookmarkResponse.PagedMedicineBookmarkResponse response = MedicineBookmarkResponse.PagedMedicineBookmarkResponse.builder()
+                .data(bookmarkResponses)
+                .paging(pagingResponse)
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 }
