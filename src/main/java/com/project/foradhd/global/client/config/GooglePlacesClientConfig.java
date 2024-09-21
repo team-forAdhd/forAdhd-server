5e2cc89c68ae9791edd50819c168cfb1a9b6010b
Daeun Kim <jkde7721@naver.com>
2024-09-21 19:56:42 +0900

    fix: 특정 feign client에 대해서만 설정 적용되도록 @Configuration 어노테이션 제거
    - @Configuration 있으면 RequestInterceptor가 전역 빈으로 등록되어 모든 feign client에 적용됨
    - @FeignClient 어노테이션의 configuration 속성 설정 시에만 적용되도록 @Configuration 어노테이션 제거
    
    #55

--- src/main/java/com/project/foradhd/global/client/config/GooglePlacesClientConfig.java
+++ src/main/java/com/project/foradhd/global/client/config/GooglePlacesClientConfig.java
@@ -3,11 +3,9 @@ package com.project.foradhd.global.client.config;
 import feign.RequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-@Configuration
 public class GooglePlacesClientConfig {
 
     private static final String HEADER_SEPARATOR = ",";
