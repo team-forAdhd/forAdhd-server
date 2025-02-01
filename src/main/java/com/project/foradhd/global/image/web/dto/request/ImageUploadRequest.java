fdee5e334758f38598f259a790e63ab7656c5605
Daeun Kim <jkde7721@naver.com>
2025-02-01 20:37:44 +0900

    fix: 이미지 업로드 api 요청 content-type 인식 잘못하는 문제 해결
    - 이미지 파일과 DTO를 @RequestPart로 각각 받는 경우 application/octet-stream으로 인식

--- src/main/java/com/project/foradhd/global/image/web/dto/request/ImageUploadRequest.java
+++ src/main/java/com/project/foradhd/global/image/web/dto/request/ImageUploadRequest.java
@@ -2,9 +2,15 @@ package com.project.foradhd.global.image.web.dto.request;
 
 import com.project.foradhd.global.image.web.enums.ImagePathPrefix;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Getter
+@Setter
 public class ImageUploadRequest {
 
-    ImagePathPrefix imagePathPrefix = ImagePathPrefix.DEFAULT_IMAGE;
+    private List<MultipartFile> imageFileList;
+    private ImagePathPrefix imagePathPrefix = ImagePathPrefix.DEFAULT_IMAGE;
 }
